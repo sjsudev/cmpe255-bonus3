2efc38a7d0d319b2438d9c968afbdbb96d494e98
sjsudev <devansh.modi@sjsu.edu>
2022-05-14 22:13:44 -0700

    model fixes

--- rest-api/app.py
+++ rest-api/app.py
@@ -3,25 +3,43 @@ from flask import Flask, jsonify, request
 import numpy as np
 import os
 import tensorflow as tf
+import re
+import nltk 
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from keras.preprocessing.sequence import pad_sequences
 
 # Initialize Flask App
 app = Flask(__name__)
+
+graph = tf.get_default_graph()
 
 def loadsavedmodel(path):
     reconstructed_model = tf.keras.models.load_model(path)
     reconstructed_model.summary()
     return reconstructed_model
 
-def inference(infermodel, img_np, class_names):
-    img_array = tf.expand_dims(img_np, 0) # Create a batch (1, 224, 224, 3)
+def preprocess(content, stem=False):
+    english_stopwords = stopwords.words('english')
 
-    predictions = infermodel.predict(img_array)#(1, 5)
-    score = tf.nn.softmax(predictions[0])#Tensor: shape=(5,)
+    stemmer = SnowballStemmer('english')
 
-    return str(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
-    )
+    regex = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"  
+    content = re.sub(regex, ' ', str(content).lower()).strip()
+    
+    tokens = []
+    
+    for token in content.split():
+        if token not in english_stopwords:
+            tokens.append(stemmer.stem(token))
+    return " ".join(tokens)
+
+def inference(infermodel, tweet):
+    predictions = infermodel.predict(tweet) #(1, 5)
+    # score = tf.nn.softmax(predictions[0])#Tensor: shape=(5,)
+    
+    return int(np.round(predictions))
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -31,8 +49,16 @@ def predict():
 
     #img_array = tfgetimagearray(args.data_path, args.img_height, args.img_width)
 
+    x = preprocess([data['tweet']])
 
-    return str(inference(model, data['tweet']))
+    with graph.as_default():
+        y = inference(model, x)
+
+        if y == 1:
+            return jsonify({'prediction': "Positive"})
+        else:
+            return jsonify({'prediction': "Negative"})
 
 if __name__ == '__main__':
     app.run()
+
