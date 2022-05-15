e416ef07b261748e248cb7d4a7864f2bdc7b03e9
sjsudev <devansh.modi@sjsu.edu>
2022-05-14 23:09:27 -0700

    update rest api

--- rest-api/app.py
+++ rest-api/app.py
@@ -9,11 +9,10 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 
 # Initialize Flask App
 app = Flask(__name__)
-
-graph = tf.get_default_graph()
 
 def loadsavedmodel(path):
     reconstructed_model = tf.keras.models.load_model(path)
@@ -22,7 +21,6 @@ def loadsavedmodel(path):
 
 def preprocess(content, stem=False):
     english_stopwords = stopwords.words('english')
-
     stemmer = SnowballStemmer('english')
 
     regex = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"  
@@ -33,13 +31,22 @@ def preprocess(content, stem=False):
     for token in content.split():
         if token not in english_stopwords:
             tokens.append(stemmer.stem(token))
-    return " ".join(tokens)
+    out = " ".join(tokens)
+
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(content)
+    max_length = 50
+
+    sequences_test = tokenizer.texts_to_sequences(content) 
+
+    return pad_sequences(sequences_test, maxlen=max_length)
+
 
 def inference(infermodel, tweet):
     predictions = infermodel.predict(tweet) #(1, 5)
     # score = tf.nn.softmax(predictions[0])#Tensor: shape=(5,)
     
-    return int(np.round(predictions))
+    return predictions
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -51,13 +58,13 @@ def predict():
 
     x = preprocess([data['tweet']])
 
-    with graph.as_default():
-        y = inference(model, x)
+    y = inference(model, x)
+    y = np.where(y>0.5, 1, 0)
 
-        if y == 1:
-            return jsonify({'prediction': "Positive"})
-        else:
-            return jsonify({'prediction': "Negative"})
+    if np.sum(y) > 1:
+        return jsonify({'prediction': "emergency"})
+    else:
+        return jsonify({'prediction': "non-emergency"})
 
 if __name__ == '__main__':
     app.run()
